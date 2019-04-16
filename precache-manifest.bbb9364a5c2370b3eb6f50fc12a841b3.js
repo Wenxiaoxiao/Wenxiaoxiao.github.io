2f90b3c541bd7659a1bddcddcc8404952697b9d0
wenxiaofang <858874032@qq.com>
2019-04-16 19:56:21 +0800

    Updates

---
+++ precache-manifest.bbb9364a5c2370b3eb6f50fc12a841b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1db7d7fc60fdea0cd15b",
+    "url": "/static/js/main.87c93789.chunk.js"
+  },
+  {
+    "revision": "7896d0712d2f77496b25",
+    "url": "/static/js/2.d9690a2e.chunk.js"
+  },
+  {
+    "revision": "7896d0712d2f77496b25",
+    "url": "/static/css/2.2b374f28.chunk.css"
+  },
+  {
+    "revision": "743b4c0bc4ed873a11075bdc1a6e7e24",
+    "url": "/index.html"
+  }
+];
