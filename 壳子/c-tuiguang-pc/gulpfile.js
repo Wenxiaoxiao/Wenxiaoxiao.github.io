e82dae2652a6b684e2fa31e1da9945f5f8da6fae
liuyuefeizhu <1879711883@qq.com>
2020-07-02 18:10:21 +0800

    no message

---
+++ 壳子/c-tuiguang-pc/gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var fileinclude  = require('gulp-file-include');
+var del = require('del');
+var cache = require('gulp-cache');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+sass.compiler = require('node-sass');
+var timestamp = (new Date()).getTime();
+gulp.task('clean', function(){
+    return del.sync(['dest/**/*' ]);
+});
+
+
+gulp.task('fileinclude', function() {
+    gulp.src(['src/*.html'])
+        .pipe(plumber())
+        .pipe(fileinclude({
+            prefix: '@@',
+            context: {
+                time: timestamp
+            },
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./dest'));
+});
+
+
+gulp.task('copy',function() {
+    return gulp.src('src/lib/**/*')
+        .pipe(gulp.dest('./dest/lib'))
+});
+
+gulp.task('copy:ico',function() {
+    return gulp.src('src/*.ico')
+        .pipe(gulp.dest('./dest'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/main/**/*.scss')
+    .pipe(plumber())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    //   .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass())
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dest/lib/css'));
+  });
+
+gulp.task('task-name', function() {
+    runSequence(['sass','fileinclude','copy','copy:ico']);
+});
+
+// Watchers
+gulp.task('watch', function() {
+    return gulp.watch([
+        'src/**/*.html',
+        'src/**/*.htm',
+        'src/lib/**',
+        'sass/**/**/*.scss',
+    ],['task-name']);
+});
+
+
+//gulp images
+gulp.task('default', ['task-name','watch']);
